dc857ace5eb0dae59b048148d943b016267233d4
ZachElf <zarchona@gmail.com>
2016-05-20 17:59:05 +0200

    Upgraded Layout_Unity
    Vissza - renamed to Menü, Added new Tovább, and Vissza buttons

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,4 +13,9 @@ public class LevelManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void LoadPreviousLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
